c8d9b3bea888294e0c26d115bb1e31ab22084cbf
Trịnh Văn Hoàng <hoang0702bmw@gmail.com>
2024-03-20 23:52:46 +0700

    add package (vueform, aos, axios, primevue, swiper, sass) & setup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
+import AOS from 'aos';
+import { onBeforeMount, onMounted } from 'vue';
+
+onBeforeMount(() => {
+    AOS.init({
+        throttleDelay: 120,
+        duration: 800,
+        offset: 0,
+        once: true,
+    });
+});
 </script>
 
 <template>
-  <RouterView />
+    <RouterView />
 </template>
