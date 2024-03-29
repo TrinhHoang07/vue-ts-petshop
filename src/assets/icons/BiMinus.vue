d29b5d0382fc6a3cbe4566a5f9455f8fa4afae13
Trịnh Văn Hoàng <hoang0702bmw@gmail.com>
2024-03-29 10:04:09 +0700

    add more icons

---
+++ src/assets/icons/BiMinus.vue
@@ -0,0 +1,21 @@
+<template>
+    <svg
+        stroke="currentColor"
+        fill="currentColor"
+        stroke-width="0"
+        viewBox="0 0 24 24"
+        height="200px"
+        width="200px"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path d="M5 11h14v2H5z"></path>
+    </svg>
+</template>
+
+<style scoped>
+svg {
+    width: 24px;
+    height: 24px;
+    color: var(--white-color);
+}
+</style>
